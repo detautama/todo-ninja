34680b811b10bfa77ad05fbf0efaef1a4a418f4d
Deta Utama <detautama11@gmail.com>
2018-12-04 07:21:16 +0800

    Lesson 3 - Basic Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is home page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
